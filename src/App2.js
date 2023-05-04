3cc80eedf16a0f9666dc4c78488f80ccf29a27ad
MiriBoyer <116269799+MiriBoyer@users.noreply.github.com>
2023-05-04 20:31:05 +0300

    change url

--- src/App2.js
+++ src/App2.js
@@ -46,7 +46,7 @@ function App2() {
       </header>
       <section className="main" style={{ display: "block" }}>
         <ul className="todo-list">
-          {todos.map(todo => {
+          {console.log(todos)&&todos.map(todo => {
             return (
               <li className={todo.isComplete ? "completed" : ""} key={todo.id}>
                 <div className="view">
