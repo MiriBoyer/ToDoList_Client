31ff81fdb22c4502a3c143462b64c0db253e9bc1
MiriBoyer <116269799+MiriBoyer@users.noreply.github.com>
2023-05-04 20:20:19 +0300

    change url

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
-console.log("111111111");
-console.log(process.env.REACT_APP_BASE_URl)
+// console.log("111111111");
+// console.log(process.env.REACT_APP_BASE_URl)
  axios.defaults.baseURL=process.env.REACT_APP_BASE_URl;
 //axios.defaults.baseURL="https://todolist-server-2469.onrender.com";
 axios.interceptors.request.use(req=>{console.log(req);
